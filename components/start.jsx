2729d1253d27ea2c4c72c0032924f18b75c1ecda
Svyatozar <konservator007@gmail.com>
2022-02-26 22:52:31 +0300

    исправил home path, добавил клик на квесты

--- components/start.jsx
+++ components/start.jsx
@@ -3,9 +3,9 @@ import { useRouter } from "next/router"
 
 export default function Start() {
     const quests = [
-        { id: 1, title: 'The Wave', description: 'You have achieved everything you dreamed of, but your world is crumbling over a catastrophe...', imgSrc: './wave.png', url: 'https://web.waym.app/quest/279' },
+        { id: 1, title: 'The Wave', description: 'You have achieved everything you dreamed of, but your world is ...', imgSrc: './wave.png', url: 'https://web.waym.app/quest/279' },
         { id: 2, title: 'President simulator', description: 'What kind of leader will you become?...', imgSrc: './president.png', url: 'https://web.waym.app/quest/306' },
-        { id: 3, title: 'In the face of danger', description: 'You found out about a catastrophe bound to happen in a year\'s time. What would you do...', imgSrc: './pandemic.png', url: 'https://web.waym.app/quest/268' },
+        { id: 3, title: 'In the face of danger', description: 'You found out about a catastrophe bound to happen in a year\'s time. What...', imgSrc: './pandemic.png', url: 'https://web.waym.app/quest/268' },
         { id: 4, title: 'First contact', description: 'What is reality? Seems like everyone has their own reality, but...', imgSrc: './first_contact.png', url: 'https://web.waym.app/quest/303' },
     ]
     const renderQuestCard = () => {
@@ -13,11 +13,13 @@ export default function Start() {
 
         return quests.map(v => {
             return (
-                <li key={v.id} href={v.url}>
-                    <div className="start__image">
+                <li key={v.id} onClick={() => {
+                    window.open(v.url, '_blank').focus();
+                }}>
+                    <div className="start__image" >
                         <img src={`${basePath}/${v.imgSrc}`} width={230} height={132} alt={v.title} />
                     </div>
-                    <div className="start__text-border">
+                    <div className="start__text-border" >
                         <div className="start__text">
                             <h4>{v.title}</h4>
                             <p>{v.description}</p>
